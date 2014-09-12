e61d35ec7cac9a31e706436955426b61110a9351
LandonWest <landonwest5@gmail.com>
2014-09-11 21:55:12 -0700

    fix migration; reseed db updated with video_urls

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912042533) do
+ActiveRecord::Schema.define(version: 20140912044209) do
 
   create_table "movies", force: true do |t|
     t.string   "title",        null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140912042533) do
     t.string   "poster_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video_url"
   end
 
   create_table "purchases", force: true do |t|
